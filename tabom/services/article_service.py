846463896e7dd6e88388103371bb8ca337ba9704
sy <phzzz@naver.com>
2022-02-15 10:14:03 +0900

    fix article_service and test_article_service

--- tabom/services/article_service.py
+++ tabom/services/article_service.py
@@ -1,16 +1,17 @@
-from django.core.paginator import Page, Paginator
 from django.db.models import Prefetch, QuerySet
 
 from tabom.models import Article, Like
 
 
-def get_an_article(article_id: int) -> Article:
-    return Article.objects.filter(id=article_id).get()
+def get_an_article(user_id: int, article_id: int) -> Article:
+    return Article.objects.prefetch_related(
+        Prefetch("like_set", queryset=Like.objects.filter(user_id=user_id), to_attr="my_likes")
+    ).get(id=article_id)
+
+    # 전 : return Article.objects.filter(id=article_id).get()
 
 
 def get_article_list(user_id: int, offset: int, limit: int) -> QuerySet[Article]:
-    # 내림차순 정렬 시 "-", 오름차순은 그냥 빈칸
-    # 쿼리셋도 슬라이싱이 가능함
     return (
         Article.objects.order_by("-id")
         .prefetch_related("like_set")
